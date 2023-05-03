e51b4fcd082cc95ec151b25bb6dfb38c47af426e
yuanx <yuanx.me@qq.com>
2023-05-03 19:10:26 +0800

    chore: clean code

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -6,44 +6,22 @@ export const config = {
 };
 
 export default function handler(request: NextRequest) {
-  try {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            height: "100%",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            backgroundColor: "white",
-            fontSize: 24,
-            fontStyle: "normal",
-            color: "black",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              width: "60%",
-            }}
-          >
-            👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá
-          </div>
-        </div>
-      ),
-      {
-        width: 800,
-        height: 400,
-      }
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
-  }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          width: "60%",
+        }}
+      >
+        👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá
+      </div>
+    ),
+    {
+      width: 800,
+      height: 400,
+    }
+  );
 }
